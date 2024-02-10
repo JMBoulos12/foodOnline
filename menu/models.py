91a52905b4cbbfdb3b21436a25d2f16ed59e8d43
Jean Marc Boulos <jmboulos@outlook.com>
2024-02-10 10:44:46 +0200

    Marketplace Implementation V2

--- menu/models.py
+++ menu/models.py
@@ -10,18 +10,21 @@ class Category(models.Model):
     description = models.TextField(max_length=250, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
-    
+
     class Meta:
         verbose_name = 'category'
         verbose_name_plural = 'categories'
-    
+
     def __str__(self):
         return self.category_name
-      
-      
+
+    def clean(self):
+        self.category_name = self.category_name.capitalize()
+
+
 class FoodItem(models.Model):
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='fooditems')
     food_title = models.CharField(max_length=50)
     slug = models.SlugField(max_length=100, unique=True)
     description = models.TextField(max_length=250, blank=True)
