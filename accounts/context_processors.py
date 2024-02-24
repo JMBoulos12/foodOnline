1dc1a3e0e38aaf1addee23819e2d437700a07e38
Jean Marc Boulos <jmboulos@outlook.com>
2024-02-24 23:01:39 +0200

    Customer profile update

--- accounts/context_processors.py
+++ accounts/context_processors.py
@@ -1,4 +1,7 @@
+from urllib.parse import uses_relative
+from accounts.models import UserProfile
 from vendor.models import Vendor
+from django.conf import settings
 
 def get_vendor(request):
     try:
@@ -6,3 +9,16 @@ def get_vendor(request):
     except:
       vendor = None
     return dict(vendor=vendor)
+  
+  
+def get_user_profile(request):
+    try:
+        user_profile = UserProfile.objects.get(user=request.user)
+    except:
+        user_profile = None
+    return dict(user_profile=user_profile)
+  
+  
+  
+def get_google_api(request):
+    return {'GOOGLE_API_KEY': settings.GOOGLE_API_KEY} # for Google Maps API v3
